4ba2e18c8e0feba69ab8195b5b624d88549e8277
mei <mei28aquarius@gmail.com>
2020-08-30 13:47:15 +0900

    ADD: ABC-177

---
+++ ABC-177/A.cpp
@@ -0,0 +1,13 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+int main() {
+    int D, T, S;
+    cin >> D >> T >> S;
+    if (T * S >= D)
+        cout << "Yes" << endl;
+    else
+        cout << "No" << endl;
+    return 0;
+}
---
+++ ABC-177/C.cpp
@@ -0,0 +1,35 @@
+#include <functional>
+#include <iostream>
+#include <bits/stdc++.h>
+
+using namespace std;
+
+void recursive_comb(int *indexes, int s, int rest,
+                    std::function<void(int *)> f) {
+    if (rest == 0) {
+        f(indexes);
+    } else {
+        if (s < 0) return;
+        recursive_comb(indexes, s - 1, rest, f);
+        indexes[rest - 1] = s;
+        recursive_comb(indexes, s - 1, rest - 1, f);
+    }
+}
+
+// nCkの組み合わせに対して処理を実行する
+void foreach_comb(int n, int k, std::function<void(int *)> f) {
+    int indexes[k];
+    recursive_comb(indexes, n - 1, k, f);
+}
+
+int main() {
+    long long N;
+    cin >> N;
+    vector<long long> A(N);
+    for (int i = 0; i < N; i++) cin >> A[i];
+
+    foreach_comb(5, 2, [](int *indexes) {
+        std::cout << indexes[0] << ',' << indexes[1] << ',' << indexes[2]
+                  << std::endl;
+    });
+}
