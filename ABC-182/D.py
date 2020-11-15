57d7666edac65b71f127d9a993219f83db32854c
mei <mei28aquarius@gmail.com>
2020-11-15 19:34:49 +0900

    ADD: some

--- ABC-182/D.py
+++ ABC-182/D.py
@@ -1,8 +1,22 @@
-N = int(input())
-A = list(map(int, input().split()))
-S = [0 for _ in range(N)]
-S[0] = A[0]
-for i in range(1, N):
-    S[i] = S[i-1] + A[i]
-print(A)
-print(S)
+n = int(input())
+a = [0] + list(map(int, input().split()))
+n += 1
+
+accum_sum = a[:]
+for i in range(1, n):
+    accum_sum[i] += accum_sum[i-1]
+
+accum_max = accum_sum[:]
+
+for i in range(1, n):
+    accum_max[i] = max(accum_max[i], accum_max[i-1])
+
+
+ans = 0
+now = 0
+
+for i in range(1, n):
+    ans = max(ans, now + accum_max[i])
+    now += accum_sum[i]
+
+print(ans)
