9d6640e7b6c243de2f51ba345ed6f0d09a324faa
mei <mei28aquarius@gmail.com>
2020-11-21 18:58:53 +0900

    ADD: some

---
+++ AGC-040/A.py
@@ -0,0 +1,17 @@
+S = input()
+
+
+a = [0] * (len(S)+1)
+
+for i, s in enumerate(S):
+    if s == '<':
+        a[i + 1] = a[i] + 1
+
+n = len(S)
+
+for i in range(n-1, -1, -1):
+    if S[i] == '>':
+        if a[i] <= a[i + 1]:
+            a[i] = a[i + 1] + 1
+
+print(sum(a))
