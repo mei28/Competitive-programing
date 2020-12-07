7eb6b54445b964ff98934bfd938d398d0515e7a0
mei <mei28aquarius@gmail.com>
2020-12-07 19:59:39 +0900

    ADD: some

---
+++ ABC-057/B.py
@@ -0,0 +1,26 @@
+N, M = map(int, input().split())
+
+A, B, C, D = [], [], [], []
+
+for _ in range(N):
+    a, b = map(int, input().split())
+    A.append(a)
+    B.append(b)
+
+for _ in range(M):
+    c, d = map(int, input().split())
+    C.append(c)
+    D.append(d)
+
+for i in range(N):
+    a, b = A[i], B[i]
+
+    min_diff = 9999999999
+    for j in range(M):
+        c, d = C[j], D[j]
+
+        diff = abs(a - c)+abs(b - d)
+        if diff < min_diff:
+            min_diff = diff
+            ans = j+1
+    print(ans)
