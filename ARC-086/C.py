5af1af57b207a53f1d4cf220e056aa01a25fe5be
mei <mei28aquarius@gmail.com>
2021-01-05 09:02:27 +0900

    ADD: some

---
+++ ARC-086/C.py
@@ -0,0 +1,22 @@
+N,K = map(int,input().split())
+A = list(map(int,input().split()))
+
+dct = {}
+key_set = set()
+for a in A:
+    dct[a] = dct.setdefault(a,0) + 1
+    key_set.add(a)
+
+dct = sorted(dct.items(), key = lambda x: x[1])
+
+diff_n = len(key_set) - K
+
+if diff_n<=0:
+    print(0)
+
+else:
+    ans = 0 
+    for i in range(diff_n):
+        ans += dct[i][1]
+    print(ans)
+
