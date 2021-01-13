f603bdfad1c25c887a4591941b17f99be81ff467
mei <mei28aquarius@gmail.com>
2021-01-13 13:14:50 +0900

    AADD: some

---
+++ ABC-234/E.py
@@ -0,0 +1,20 @@
+from collections import deque
+x = int(input())
+
+que = deque(range(1,100))
+
+while que:
+    num = que.popleft()
+
+    if num>=x:
+        print(num)
+        exit()
+    
+    if num >= 10:
+        nf = num % 10
+        ns = (num//10)%10
+        d = nf -ns
+        t = nf +d 
+        if 0<=t<=9:
+            que.append(num*10+t)
+
