64dedd9df93a3ab04985c1f7f42c9d134d43d4b4
mei <mei28aquarius@gmail.com>
2021-02-05 18:38:15 +0900

    ADD: some

---
+++ ABC-131/C.py
@@ -0,0 +1,15 @@
+def gcd(a,b):
+    while b:
+        a,b = b, a%b
+    return a
+
+def lcm(a,b):
+    return a * b // gcd(a,b)
+
+A,B,C,D = map(int,input().split())
+
+b_num = B//C + B//D - B//(lcm(C,D))
+a_num = (A-1)//C + (A-1)//D - (A-1)//(lcm(C,D))
+
+print((B-A+1)-(b_num-a_num))
+
