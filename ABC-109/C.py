d3535e55b8af8be1a0d4bb6a8cf366383e9faa8e
mei <mei28aquarius@gmail.com>
2021-02-07 23:20:31 +0900

    ADD: some

---
+++ ABC-109/C.py
@@ -0,0 +1,18 @@
+N, X = map(int,input().split())
+
+A = list(map(int,input().split()))
+
+D = []
+
+for a in A:
+    D.append(abs(X-a))
+
+def gcd(a,b):
+    while b:
+        a,b = b, a%b
+    return a
+
+ans = D[0]
+for d in D:
+    ans = gcd(ans,d)
+print(ans)
