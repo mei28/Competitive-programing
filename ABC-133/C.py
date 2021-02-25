1c0ddede025567c1b1a463fe4b8ee5315abe221f
mei <mei28aquarius@gmail.com>
2021-02-26 07:07:03 +0900

    ADD: some

--- ABC-133/C.py
+++ ABC-133/C.py
@@ -1,11 +1,23 @@
 L, R = map(int, input().split())
-
-if abs(R - L) >= 2019:
-    print(0)
+if R - L + 1 >= 2020:
+    ans = 0
+    
 else:
-    ans = 2018
-    for i in range(L, R):
-        for j in range(i + 1, R + 1):
-            ans = min(ans, (i * j) % 2019)
-
+    List = [i%2019 for i in range(L, R+1)]
+    #a = min(List)
+    #List.remove(a)
+    #ans = (a*(min(List))) % 2019
+    #sorted(List)
+    #ans = List[0]*List[1] % 2019
+    if 0 in List:
+        ans = 0
+    else:
+        m = float("inf")
+        for i in range(len(List)):
+            for j in range(i+1, len(List)):
+                if List[i]*List[j] % 2019 < m:
+                    m = List[i]*List[j] % 2019
+        ans = m
+                
+                
 print(ans)
