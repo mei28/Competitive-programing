59ac7f926e93e0ce45c383210333e29a522893cf
mei <mei28aquarius@gmail.com>
2021-03-03 20:30:20 +0900

    ADD: some

---
+++ ABC-094/C.py
@@ -0,0 +1,9 @@
+n = int(input())
+x = list(map(int, input().split()))
+y = sorted(x)
+
+for i in range(n):
+    if x[i] < y[n // 2]:
+        print(y[n // 2])
+    else:
+        print(y[n // 2 - 1])
