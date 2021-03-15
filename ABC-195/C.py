2e61a9bb1f928eeccb34da54bbb9f2b7bf54d6d1
mei <mei28aquarius@gmail.com>
2021-03-15 10:15:40 +0900

    ADD: some'

---
+++ ABC-195/C.py
@@ -0,0 +1,10 @@
+N = int(input())
+
+ans = 0
+ans += max(0, (N - 10 ** 3) + 1)
+ans += max(0, (N - 10 ** 6) + 1)
+ans += max(0, (N - 10 ** 9) + 1)
+ans += max(0, (N - 10 ** 12) + 1)
+ans += max(0, (N - 10 ** 15) + 1)
+
+print(ans)
