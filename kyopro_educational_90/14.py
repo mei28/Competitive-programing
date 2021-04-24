46f8a50d89096cb1c8aef117b38c292d0efecddd
mei <mei28aquarius@gmail.com>
2021-04-24 14:36:08 +0900

    ADD: some

---
+++ kyopro_educational_90/14.py
@@ -0,0 +1,14 @@
+if __name__ == '__main__':
+    N = int(input())
+    A = list(map(int,input().split()))
+    B = list(map(int,input().split()))
+    A.sort()
+    B.sort()
+
+    ans = 0
+    for i in range(N):
+        a,b = A[i], B[i]
+        ans += abs(a-b)
+    print(ans)
+    pass
+
