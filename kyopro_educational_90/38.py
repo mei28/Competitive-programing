08390293a9a72352ee9ac527a84ec547816573af
mei <mei28aquarius@gmail.com>
2021-05-12 15:37:45 +0900

    ADD: gcd

---
+++ kyopro_educational_90/38.py
@@ -0,0 +1,18 @@
+def gcd(a, b):
+    if b == 0:
+        return a
+    return gcd(b, a % b)
+
+
+def lcm(a, b):
+    return a * b // gcd(a, b)
+
+
+if __name__ == "__main__":
+    a, b = map(int, input().split())
+
+    tmp = lcm(a, b)
+    if tmp > 1e18:
+        print("Large")
+    else:
+        print(tmp)
