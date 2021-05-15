56464406fad4dceeaff6048cabe9f7d95ae5a281
mei <mei28aquarius@gmail.com>
2021-05-15 12:23:45 +0900

    ADD: bin search and syakutori

---
+++ ABC-143/D.py
@@ -0,0 +1,25 @@
+def binary_search(q: int, left: int):
+    ok = left
+    ng = N
+    while ng - ok > 1:
+        mid = (ok + ng) // 2
+        if L[mid] < q:
+            ok = mid
+        else:
+            ng = mid
+    return ok
+
+
+if __name__ == "__main__":
+
+    N = int(input())
+    L = list(map(int, input().split()))
+    L.sort()
+
+    ans = 0
+    for i in range(0, N - 2):
+        for j in range(i + 1, N - 1):
+            k = binary_search(L[i] + L[j], j)
+            ans += max(0, k - j)
+
+    print(ans)
---
+++ ABC-143/D_1.py
@@ -0,0 +1,16 @@
+if __name__ == "__main__":
+
+    N = int(input())
+    L = list(map(int, input().split()))
+    L.sort()
+
+    ans = 0
+    # しゃくとりほう
+    for i in range(N):
+        k = i
+        for j in range(i + 1, N):
+            while k < N and L[k] < L[i] + L[j]:
+                k += 1
+            ans += k - (j + 1)
+
+    print(ans)
