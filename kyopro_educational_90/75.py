99c45ed0515ec8ea888efb098fd09b8481502429
mei <mei28aquarius@gmail.com>
2021-06-24 20:44:51 +0900

    ADD: factorial

---
+++ kyopro_educational_90/75.py
@@ -0,0 +1,23 @@
+n = int(input())
+primes = dict()
+
+for denominator in range(2, int(n ** 0.5) + 1):
+    while n % denominator == 0:
+        n //= denominator
+        primes[denominator] = primes.setdefault(denominator, 0) + 1
+
+if n != 1:
+    primes[n] = primes.setdefault(n, 0) + 1
+
+cnt = 0
+for k, v in primes.items():
+    cnt += v
+
+if cnt == 1:
+    print(0)
+else:
+    ans = 0
+    while cnt > 1:
+        ans += 1
+        cnt = -(-cnt // 2)
+    print(ans)
