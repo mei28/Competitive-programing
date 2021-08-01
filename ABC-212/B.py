15fba731af68432da6324198c699fe1c69d46200
mei <mei28aquarius@gmail.com>
2021-08-01 18:52:02 +0900

    ADD: some

--- ABC-212/B.py
+++ ABC-212/B.py
@@ -1,4 +1,1 @@
-X = input()
-print("Weak") if len(set(X)) == 1 else print("Weak") if all(
-    (int(X[i]) + 1) % 10 == int(X[i + 1]) for i in range(3)
-) else print("Strong")
+print("Weak") if len(set(X := input())) == 1 else print("Weak") if all((int(X[i]) + 1) % 10 == int(X[i + 1]) for i in range(3)) else print("Strong")
