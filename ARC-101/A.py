51098eb6ffa34f75348abd56bcbcd7fb7622a804
mei <mei28aquarius@gmail.com>
2021-08-03 17:16:00 +0900

    ADD: some

---
+++ ARC-101/A.py
@@ -0,0 +1,16 @@
+n, k = map(int, input().split())
+X = list(map(int, input().split()))
+
+res = 10 ** 10
+
+for left in range(n - k + 1):
+    right = left + k - 1
+
+    lr = abs(X[right] - X[left])
+
+    l_f = abs(X[left]) + lr
+    r_f = abs(X[right]) + lr
+
+    res = min(res, l_f, r_f)
+
+print(res)
