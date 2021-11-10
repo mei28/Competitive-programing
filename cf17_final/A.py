af02325e6a10c2ad54b08a89eeb588323c7f309c
mei <mei28aquarius@gmail.com>
2021-11-10 12:58:14 +0900

    ADD: some

---
+++ cf17_final/A.py
@@ -0,0 +1,23 @@
+s = input()
+correct = [
+        'KIHBR',
+        'KIHBRA',
+        'KIHBAR',
+        'KIHBARA',
+        'KIHABR',
+        'KIHABRA',
+        'KIHABAR',
+        'KIHABARA',
+        'AKIHBR',
+        'AKIHBRA',
+        'AKIHBAR',
+        'AKIHBARA',
+        'AKIHABR',
+        'AKIHABRA',
+        'AKIHABAR',
+        'AKIHABARA',
+        ]
+if s in correct:
+    print("YES")
+else:
+    print('NO')
