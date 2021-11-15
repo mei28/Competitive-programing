fa5c4d01cb1e2e1ec362291aa6be408be85b3d9c
mei <mei28aquarius@gmail.com>
2021-11-15 14:30:25 +0900

    ADD: some

---
+++ ARC-127/A.py
@@ -0,0 +1,25 @@
+n: int = int(input())
+
+result: int = 0
+
+for i in range(1, 16):
+    start = int("1" * i)
+
+    if start > n:
+        break
+    else:
+        result += 1
+
+    bottom = top = start
+
+    while True:
+        bottom *= 10
+
+        top = (top + 1) * 10 - 1
+
+        if n < bottom:
+            break
+
+        result += min(n,top) - bottom + 1
+
+print(result)
