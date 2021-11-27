34b92d69d1d90ef83771c6a02e2d53dd7cd9a3f9
mei <mei28aquarius@gmail.com>
2021-11-27 23:25:25 +0900

    ADD: binary search + ruisekiwa

--- ABC-229/D.py
+++ ABC-229/D.py
@@ -5,13 +5,18 @@ k = int(input())
 ok = 0
 ng = len(S) + 1
 
+A = [0]*(len(S)+1)
+for i in range(len(S)):
+    A[i+1] = A[i]
+    if S[i]==".":
+        A[i+1] += 1
+
 def check(ans:int)->bool:
     """
     kこ書き換えて連続してansすることがあるか
     """
     for i in range(len(S)-ans+1):
-        tmp = S[i:i+ans]
-        if tmp.count('.') <= k:
+        if A[i+ans] - A[i] <= k:
             return True
     return False
 
