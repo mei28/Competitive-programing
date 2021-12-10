7f71680f9c67b19619349d5475d4a4b887e8e318
mei <mei28aquarius@gmail.com>
2021-12-10 11:10:59 +0900

    ADD: some

---
+++ ABC-152/D.py
@@ -0,0 +1,19 @@
+n = int(input())
+
+# cnt[i][j] : 先頭がi，末尾がjである個数
+
+cnt = [[0]*10 for _ in range(10)]
+for i in range(1,n+1):
+    head = int(str(i)[0])
+    tail = int(str(i)[-1])
+
+    cnt[head][tail]+= 1
+
+
+ans = 0
+
+for i in range(10):
+    for j in range(10):
+        ans += cnt[i][j] * cnt[j][i]
+
+print(ans)
