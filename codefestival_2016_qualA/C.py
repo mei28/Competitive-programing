9991a21ce2650297cc7497cf8ebae9748ddd10cf
mei <mei28aquarius@gmail.com>
2021-12-11 13:19:39 +0900

    ADD: some

---
+++ codefestival_2016_qualA/C.py
@@ -0,0 +1,16 @@
+S = [i for i in input()]
+k = int(input())
+
+for i in range(len(S)):
+    condition = ( (ord("a")-ord(S[i]))+26 )%26
+
+    if condition <= k:
+        S[i] = 'a'
+        k -= condition
+
+    if i== len(S)-1:
+        k%= 26
+        S[i] = chr( ord(S[i])+k )
+
+print(''.join(S))
+
