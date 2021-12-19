983811069423b0298ca4a2d2d9141067a42aaf4c
mei <mei28aquarius@gmail.com>
2021-12-19 20:16:16 +0900

    ADD: some

---
+++ ARC-003/B.py
@@ -0,0 +1,14 @@
+n = int(input())
+
+l = []
+
+for i in range(n):
+    s = input()
+    rev_s = s[::-1]
+    l.append((s,rev_s))
+
+
+l = list(sorted(l,key=lambda x: x[1]))
+
+for i in l:
+    print(i[0])
