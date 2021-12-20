6e292a2069bd2540dd8dd172b497c91d4db9e4da
mei <mei28aquarius@gmail.com>
2021-12-20 14:45:49 +0900

    ADD: some

--- ABC-224/D.py
+++ ABC-224/D.py
@@ -1,12 +1,28 @@
 m = int(input())
-G = [[0] * m for _ in range(m)]
+edge = [[] for _ in range(9)]
 
 for _ in range(m):
     u, v = map(int, input().split())
     u -= 1
     v -= 1
 
-    G[u][v] = 1
-    G[v][u] = 1
+    edge[u].append(v)
+    edge[v].append(u)
 
-P = map(int, input().split())
+P = list(map(lambda x: int(x) - 1, input().split()))
+used = [True] * 9
+for place in P:
+    used[place] = False
+empty = 0
+while not used[empty]:
+    empty += 1
+P.append(empty)
+
+goal = 123456789
+
+dist = {P: 0}
+vec = [P]
+
+while len(vec)!=0:
+    new_vec = []
+    for 
