77eb444172ccfae95f6edf720a0bfde87be8ab7c
mei <mei28aquarius@gmail.com>
2021-12-29 17:48:22 +0900

    ADD: some

---
+++ ABC-233/E.py
@@ -0,0 +1,21 @@
+x = input()
+A = list(map(int,x))
+S = [0]*(len(x)+1)
+for i in range(len(x)):
+    S[i+1] = S[i] + A[i]
+
+S = S[::-1]
+res = []
+rem = 0
+
+for x in S:
+    rem += x
+    res.append(rem%10)
+    rem //=10
+while rem > 0:
+    res.append(rem%10)
+    rem//=10
+
+ans = res[::-1]
+print(*ans,sep='')
+
