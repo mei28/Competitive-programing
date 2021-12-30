3e9170050ff54ea6a156518937823ced50eaecde
mei <mei28aquarius@gmail.com>
2021-12-30 10:29:30 +0900

    ADD: some

---
+++ ARC-132/A.py
@@ -0,0 +1,14 @@
+n = int(input())
+R = list(map(int,input().split()))
+C = list(map(int,input().split()))
+
+q = int(input())
+ans = []
+for i in range(q):
+    r,c = map(lambda x: int(x)-1,input().split())
+    if R[r]+C[c]>=n+1:
+        ans.append("#")
+    else:
+        ans.append('.')
+
+print(*ans,sep='')
