f002c335460e6bb1650d42433e2c009b748eab1e
mei <mei28aquarius@gmail.com>
2022-01-14 13:44:38 +0900

    ADD: dfs

---
+++ panasonic2020/D.py
@@ -0,0 +1,11 @@
+n:  int = int(input())
+
+def dfs(s:str,mx):
+    if len(s) ==n:
+        print(s)
+        return 
+    
+    for i in range(ord('a'),ord(mx)+1):
+        dfs(s+chr(i), chr(ord(mx)+1)if chr(i)==mx else mx)
+
+dfs('','a')
