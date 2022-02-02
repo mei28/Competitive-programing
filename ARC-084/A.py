49e064e07b7453527758e920836bebd0096f5cfc
mei <mei28aquarius@gmail.com>
2022-02-02 17:38:32 +0900

    add bisect

---
+++ ARC-084/A.py
@@ -0,0 +1,20 @@
+from bisect import bisect_left,bisect_right
+
+n = int(input())
+A = list(map(int,input().split()))
+A.sort()
+B = list(map(int,input().split()))
+B.sort()
+C = list(map(int,input().split()))
+C.sort()
+
+ans = 0
+
+
+for b in B:
+    a_ = bisect_left(A,b)
+    c_ = len(C)-bisect_right(C,b)
+
+    ans += a_ * c_
+
+print(ans)
