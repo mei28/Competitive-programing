6c3ad527042acf6a4228ef2fd57d53dc14ab463b
mei <mei28aquarius@gmail.com>
2022-02-13 19:44:12 +0900

    add some

--- AGC-038/A.py
+++ AGC-038/A.py
@@ -3,10 +3,10 @@ h,w,a,b = map(int,input().split())
 for i in range(h):
     for j in range(w):
         if i<b and j<a:
-            print('0', end=' ')
+            print('0', end='')
         elif i>=b and j>=a:
-            print('0',end =' ')
+            print('0',end ='')
         else:
-            print('1',end = ' ')
+            print('1',end = '')
 
     print('')
