42293af274301dcdc65b1eb4fe4c3f5defb92ea2
mei <mei28aquarius@gmail.com>
2022-02-14 14:29:46 +0900

    add dfs memo

---
+++ ARC-135/A.py
@@ -0,0 +1,16 @@
+x = int(input())
+MOD = 998244353
+
+
+M = dict()
+
+def dfs(_x:int)->int:
+    if _x in M.keys():
+        return M[_x]
+    if _x < 4:
+        return _x
+
+    M[_x] = (dfs(_x//2) * dfs(-(-_x//2))) % MOD
+    return M[_x]
+
+print(dfs(x))
