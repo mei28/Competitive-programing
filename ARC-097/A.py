e709528d9137e30d7511686f40bbb1eb5d94a0be
mei <mei28aquarius@gmail.com>
2022-02-16 11:39:23 +0900

    add some

---
+++ ARC-097/A.py
@@ -0,0 +1,15 @@
+s = input()
+k = int(input())
+n = len(s)
+
+se = set()
+
+for i in range(n):
+    for j in range(1,6):
+        if i+j >n:
+            continue
+        se.add(s[i:i+j])
+
+li = list(se)
+li.sort()
+print(li[k-1])
