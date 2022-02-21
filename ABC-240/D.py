1378d1af9c992fd69dbb2db45ce096c62ad7550b
mei <mei28aquarius@gmail.com>
2022-02-21 14:32:58 +0900

    add runlength

--- ABC-240/D.py
+++ ABC-240/D.py
@@ -1,26 +1,17 @@
-from collections import deque,defaultdict
 n = int(input())
-
-Q = deque()
-
 A = list(map(int,input().split()))
-
-ans = 0
-last_a = 1
-for i in range(n):
-    a = A[i]
-    Q.append(a)
-    print(Q,end=' ')
-    if last_a == a:
-        while len(Q)>=0:
-            q = Q.pop()
-            if q!=a:
-                # もどす
-                Q.append(q)
-                break
-    last_a  = a
-    print(Q)
-    print(len(Q))
+stack = []
 
 
+cnt = 0
+for a in A:
+    if len(stack) != 0 and stack[-1][0] == a:
+        stack[-1][1] += 1
+    else:
+        stack.append([a,1])
+    cnt += 1
+    if stack[-1][0] == stack[-1][1]:
+        cnt -= stack[-1][0]
+        stack.pop()
+    print(cnt)
 
