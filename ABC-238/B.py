d9f4a73cc0311201a798fb7ce507e7004cad2322
mei <mei28aquarius@gmail.com>
2022-02-22 17:30:16 +0900

    add some

--- ABC-238/B.py
+++ ABC-238/B.py
@@ -1,9 +1,9 @@
 n = int(input())
-A = list(map(int,input().split()))
+A = list(map(int, input().split()))
 
 S = [0]
 for a in A:
-    t = (S[-1]+a) % 360
+    t = (S[-1] + a) % 360
     S.append(t)
 
 S.sort()
@@ -11,10 +11,9 @@ S.sort()
 ans = 0
 
 for i in range(len(S)):
-    if S[i]==S[-1]:
-        ans = max(ans,360-S[i])
+    if S[i] == S[-1]:
+        ans = max(ans, 360 - S[i])
     else:
-        ans = max(ans, S[i+1]-S[i])
+        ans = max(ans, S[i + 1] - S[i])
 
 print(ans)
-
--- ABC-238/D.py
+++ ABC-238/D.py
@@ -1,19 +1,18 @@
 T = int(input())
-A,S = [],[]
+A, S = [], []
 for _ in range(T):
-    a,s = map(int,input().split())
+    a, s = map(int, input().split())
     A.append(a)
     S.append(s)
 
 
-
-for a,s in zip(A,S):
-    t = s-2*a
-    if t<0:
-        print('No')
+for a, s in zip(A, S):
+    t = s - 2 * a
+    if t < 0:
+        print("No")
         continue
 
-    if t& a==0:
-        print('Yes')
+    if t & a == 0:
+        print("Yes")
     else:
-        print('No')
+        print("No")
