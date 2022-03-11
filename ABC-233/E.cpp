5d3572c0b7f5d14cae06b6750ead7a1673d8cd0e
mei <mei28aquarius@gmail.com>
2022-03-11 11:40:26 +0900

    　add some

---
+++ ABC-233/E.cpp
@@ -0,0 +1,31 @@
+#include <iostream>
+#include <string>
+#include <vector>
+
+using namespace std;
+using Int = long long;
+
+using namespace std;
+int main() {
+  string x;
+  cin >> x;
+  int s = 0, c = 0;
+  for (auto &nx : x) {
+    s += (nx - '0');
+  }
+  string res;
+  for (int i = x.size() - 1;; i--) {
+    c += s;
+    res.push_back('0' + (c % 10));
+    c /= 10;
+    if (i >= 0) {
+      s -= (x[i] - '0');
+    }
+    if (i <= 0 && c == 0) {
+      break;
+    }
+  }
+  reverse(res.begin(), res.end());
+  cout << res << '\n';
+  return 0;
+}
