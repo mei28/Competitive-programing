6b7dbd68df1153ab84a64e7f85d418753dd62165
mei <mei28aquarius@gmail.com>
2022-03-26 22:42:51 +0900

    add some

---
+++ ABC-245/C.py
@@ -0,0 +1,25 @@
+n,k = map(int,input().split())
+A = list(map(int,input().split()))
+B = list(map(int,input().split()))
+
+dp = [[False,False] for _ in range(n+2)]
+dp[0] = [True,True]
+
+for i in range(n-1):
+    for j in range(2):
+        if dp[i][j]:
+            if j==0:
+                a = A[i]
+            else:
+                a = B[i]
+            if abs(a-A[i+1]) <=k:
+                dp[i+1][0] = True
+            if abs(a-B[i+1]) <=k:
+                dp[i+1][1] = True
+
+
+# print(dp)
+if dp[n-1][0] or dp[n-1][1]:
+    print('Yes')
+else:
+    print('No')
---
+++ ABC-245/D.py
@@ -0,0 +1,24 @@
+n,m = map(int,input().split())
+A = list(map(int,input().split()))[::-1]
+C = list(map(int,input().split()))[::-1]
+
+
+B = []
+for i in range(n+m):
+    # print(A)
+    # print(B)
+    # print(C)
+    # print('_'*20)
+    # C
+    for j in range(n):
+        # A
+        if A[j]==0:
+            continue
+        s = C[i+j]//A[j]
+        B.append(s)
+        C[i] =0
+        for k in range(j+1,n+1):
+            C[i+k] -= A[k]*s
+
+
+print(*B[::-1])
