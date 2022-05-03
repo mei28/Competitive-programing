347aa89848255d7a33eaa67d339eaffc1e6e3765
mei <mei28aquarius@gmail.com>
2022-05-03 09:49:56 +0900

    add some

---
+++ ARC-139/A.py
@@ -0,0 +1,11 @@
+n = int(input())
+T = list(map(int,input().split()))
+
+ans = 0
+for i,t in enumerate(T):
+    pt = 1<<t
+    ans = (ans//pt+1)*pt
+    if ans %(pt*2) == 0:
+        ans += pt
+
+print(ans)
