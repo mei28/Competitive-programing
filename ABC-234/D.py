0fa36f6657096a47be2dfe5525ddf3afb8d9e67b
mei <mei28aquarius@gmail.com>
2022-05-27 09:19:28 +0900

    add heapq

--- ABC-234/D.py
+++ ABC-234/D.py
@@ -1,2 +1,14 @@
-n,k = map(int,input().split())
-P= map(int,input().split())
+from heapq import heapify, heappop, heappush
+
+n, k = map(int, input().split())
+P = list(map(int, input().split()))
+pq = P[:k]
+
+heapify(pq)
+
+print(pq[0])
+
+for x in P[k:]:
+    heappush(pq, x)
+    heappop(pq)
+    print(pq[0])
--- ABC-248/C.py
+++ ABC-248/C.py
@@ -1,21 +1,21 @@
 MOD = 998244353
-n,m,k=map(int,input().split())
+n, m, k = map(int, input().split())
 
-dp = [[0]*(k+1) for _ in range(n+1)]
+dp = [[0] * (k + 1) for _ in range(n + 1)]
 # dp[i][j]：先頭iこうまでで合計がjになるやつの合計数
 
 dp[0][0] = 1
 
 for i in range(n):
-    for j in range(k+1):
-        for o in range(1,m+1):
-            if j+o <=k:
-                dp[i+1][j+o] += dp[i][j]
-                dp[i+1][j+o] %= MOD
+    for j in range(k + 1):
+        for o in range(1, m + 1):
+            if j + o <= k:
+                dp[i + 1][j + o] += dp[i][j]
+                dp[i + 1][j + o] %= MOD
 
 ans = 0
 
-for i in range(k+1):
+for i in range(k + 1):
     ans += dp[n][i]
     ans %= MOD
 
