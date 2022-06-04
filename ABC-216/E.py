a308b19d1db967814a46fe7529343dee24ac6476
mei <mei28aquarius@gmail.com>
2022-06-04 15:26:07 +0900

    add binary

---
+++ ABC-216/E.py
@@ -0,0 +1,37 @@
+def main():
+    def judge(m):
+        cnt = 0
+        for x in A:
+            if x >= m:
+                cnt += x - m + 1
+        return cnt <= K
+
+    def solve(m):
+        rem = K
+        ans = 0
+        for i in range(N):
+            x = A[i]
+            if x >= m:
+                diff = x - m + 1
+                ans += diff * (x + m) // 2  # 等差数列の和の公式
+                rem -= diff
+        ans += rem * (m - 1)
+        return ans
+
+    N, K = map(int, input().split())
+    A = list(map(int, input().split()))
+
+    ok = 2 * 10 ** 9 + 5
+    ng = 0
+
+    while abs(ok - ng) > 1:
+        mid = (ok + ng) // 2
+        if judge(mid):
+            ok = mid
+        else:
+            ng = mid
+    print(solve(ok))
+
+
+if __name__ == "__main__":
+    main()
