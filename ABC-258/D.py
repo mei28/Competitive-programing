ae62b9ce6bb8c5a854b6f944acc72f0076c484fe
mei <mei28aquarius@gmail.com>
2022-07-03 10:33:15 +0900

    add some

--- ABC-258/D.py
+++ ABC-258/D.py
@@ -1,5 +1,21 @@
 n, x = map(int, input().split())
-M = []
+A = []
+B = []
+B_min = []
 for _ in range(n):
     a, b = map(int, input().split())
-    M.append([a, b])
+    A.append(a)
+    B.append(b)
+    B_min.append(b)
+
+for i in range(1, n):
+    A[i] += A[i - 1]
+    B[i] += B[i - 1]
+    B_min[i] = min(B_min[i], B_min[i - 1])
+
+ans = 10 ** 30
+for i in range(n):
+    tmp = A[i] + B[i] + B_min[i] * max(0, (x - (i + 1)))
+    ans = min(ans, tmp)
+
+print(ans)
