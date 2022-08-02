a4bf8686e8df2d3e66c4762ed5ad9010e921faa3
mei <mei28aquarius@gmail.com>
2022-08-02 21:44:10 +0900

    sorted list

--- ABC-260/D.py
+++ ABC-260/D.py
@@ -1,69 +1,170 @@
-from collections import defaultdict
+# https://github.com/tatyam-prime/SortedSet/blob/main/SortedSet.py
+import math
 from bisect import bisect_left, bisect_right
+from typing import Generic, Iterable, Iterator, List, TypeVar, Union
+
+T = TypeVar("T")
 
 
-class UnionFind:
-    def __init__(self, n):
-        self.n = n
-        self.parents = [-1] * n
+class SortedSet(Generic[T]):
+    BUCKET_RATIO = 50
+    REBUILD_RATIO = 170
 
-    def find(self, x):
-        if self.parents[x] < 0:
-            return x
-        else:
-            self.parents[x] = self.find(self.parents[x])
-            return self.parents[x]
+    def _build(self, a=None) -> None:
+        "Evenly divide `a` into buckets."
+        if a is None:
+            a = list(self)
+        size = self.size = len(a)
+        bucket_size = int(math.ceil(math.sqrt(size / self.BUCKET_RATIO)))
+        self.a = [
+            a[size * i // bucket_size : size * (i + 1) // bucket_size]
+            for i in range(bucket_size)
+        ]
 
-    def union(self, x, y):
-        x = self.find(x)
-        y = self.find(y)
+    def __init__(self, a: Iterable[T] = []) -> None:
+        "Make a new SortedSet from iterable. / O(N) if sorted and unique / O(N log N)"
+        a = list(a)
+        if not all(a[i] < a[i + 1] for i in range(len(a) - 1)):
+            a = sorted(set(a))
+        self._build(a)
 
-        if x == y:
-            return
+    def __iter__(self) -> Iterator[T]:
+        for i in self.a:
+            for j in i:
+                yield j
 
-        if self.parents[x] > self.parents[y]:
-            x, y = y, x
+    def __reversed__(self) -> Iterator[T]:
+        for i in reversed(self.a):
+            for j in reversed(i):
+                yield j
 
-        self.parents[x] += self.parents[y]
-        self.parents[y] = x
+    def __len__(self) -> int:
+        return self.size
 
-    def size(self, x):
-        return -self.parents[self.find(x)]
+    def __repr__(self) -> str:
+        return "SortedSet" + str(self.a)
 
-    def same(self, x, y):
-        return self.find(x) == self.find(y)
+    def __str__(self) -> str:
+        s = str(list(self))
+        return "{" + s[1 : len(s) - 1] + "}"
 
-    def members(self, x):
-        root = self.find(x)
-        return [i for i in range(self.n) if self.find(i) == root]
+    def _find_bucket(self, x: T) -> List[T]:
+        "Find the bucket which should contain x. self must not be empty."
+        for a in self.a:
+            if x <= a[-1]:
+                return a
+        return a
 
-    def roots(self):
-        return [i for i, x in enumerate(self.parents) if x < 0]
+    def __contains__(self, x: T) -> bool:
+        if self.size == 0:
+            return False
+        a = self._find_bucket(x)
+        i = bisect_left(a, x)
+        return i != len(a) and a[i] == x
 
-    def group_count(self):
-        return len(self.roots())
+    def add(self, x: T) -> bool:
+        "Add an element and return True if added. / O(√N)"
+        if self.size == 0:
+            self.a = [[x]]
+            self.size = 1
+            return True
+        a = self._find_bucket(x)
+        i = bisect_left(a, x)
+        if i != len(a) and a[i] == x:
+            return False
+        a.insert(i, x)
+        self.size += 1
+        if len(a) > len(self.a) * self.REBUILD_RATIO:
+            self._build()
+        return True
 
-    def all_group_members(self):
-        group_members = defaultdict(list)
-        for member in range(self.n):
-            group_members[self.find(member)].append(member)
-        return group_members
+    def discard(self, x: T) -> bool:
+        "Remove an element and return True if removed. / O(√N)"
+        if self.size == 0:
+            return False
+        a = self._find_bucket(x)
+        i = bisect_left(a, x)
+        if i == len(a) or a[i] != x:
+            return False
+        a.pop(i)
+        self.size -= 1
+        if len(a) == 0:
+            self._build()
+        return True
 
-    def __str__(self):
-        return "\n".join(f"{r}: {m}" for r, m in self.all_group_members().items())
+    def lt(self, x: T) -> Union[T, None]:
+        "Find the largest element < x, or None if it doesn't exist."
+        for a in reversed(self.a):
+            if a[0] < x:
+                return a[bisect_left(a, x) - 1]
 
+    def le(self, x: T) -> Union[T, None]:
+        "Find the largest element <= x, or None if it doesn't exist."
+        for a in reversed(self.a):
+            if a[0] <= x:
+                return a[bisect_right(a, x) - 1]
+
+    def gt(self, x: T) -> Union[T, None]:
+        "Find the smallest element > x, or None if it doesn't exist."
+        for a in self.a:
+            if a[-1] > x:
+                return a[bisect_right(a, x)]
+
+    def ge(self, x: T) -> Union[T, None]:
+        "Find the smallest element >= x, or None if it doesn't exist."
+        for a in self.a:
+            if a[-1] >= x:
+                return a[bisect_left(a, x)]
+
+    def __getitem__(self, x: int) -> T:
+        "Return the x-th element, or IndexError if it doesn't exist."
+        if x < 0:
+            x += self.size
+        if x < 0:
+            raise IndexError
+        for a in self.a:
+            if x < len(a):
+                return a[x]
+            x -= len(a)
+        raise IndexError
+
+    def index(self, x: T) -> int:
+        "Count the number of elements < x."
+        ans = 0
+        for a in self.a:
+            if a[-1] >= x:
+                return ans + bisect_left(a, x)
+            ans += len(a)
+        return ans
+
+    def index_right(self, x: T) -> int:
+        "Count the number of elements <= x."
+        ans = 0
+        for a in self.a:
+            if a[-1] > x:
+                return ans + bisect_right(a, x)
+            ans += len(a)
+        return ans
+
+
+################################
 
 n, k = map(int, input().split())
-P = list(map(int, input().split()))
+ans = [-1] * n
+from collections import defaultdict
 
-U = UnionFind(n)
-ba = []
+yama = defaultdict(list)
+s = SortedSet()
 
-for i, p in enumerate(P):
-    if len(ba) == 0:
-        ba.append(p)
-        ba.sort()
-    idx = bisect_right(ba, p)
-
-    if idx < len(ba):
-        U.union(i, idx + 1)
+for i, x in enumerate(map(int, input().split())):
+    key = s.ge(x)
+    if key is not None:
+        yama[x] = yama.pop(key)
+        s.discard(key)
+    yama[x].append(x)
+    s.add(x)
+    if len(yama[x]) == k:
+        for y in yama.pop(x):
+            ans[y - 1] = i + 1
+        s.discard(x)
+print(*ans)
