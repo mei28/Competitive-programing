d17c4b3db5b62d89407e202d331263370c837672
mei <mei28aquarius@gmail.com>
2023-01-10 21:12:24 +0900

    add LIS

---
+++ tessoku_book/X.py
@@ -0,0 +1,16 @@
+from bisect import bisect_left
+
+n = int(input())
+A = list(map(int, input().split()))
+INF = float("inf")
+dp = [INF] * (n + 2)
+# dp[i]: 長さがiの最小値
+
+for a in A:
+    idx = bisect_left(dp, a)
+    dp[idx] = a
+ans = 0
+while dp[ans] != INF:
+    a += 1
+
+print(ans)
