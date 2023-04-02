1e577a88dcc69edabfdfe67812f56d7ac222f979
mei <mei28aquarius@gmail.com>
2023-04-02 20:22:44 +0900

    add some

--- algoshiki/1114.py
+++ algoshiki/1114.py
@@ -1,24 +1,34 @@
-n, a = map(int, input().split())
+# 入力を受け取る
+N, A = map(int, input().split())
 P = list(map(int, input().split()))
 Q = list(map(int, input().split()))
 R = list(map(int, input().split()))
 
-# dp[i][j] = i日目に店舗jで買った時
+S = [P, Q, R]
+
+# DP テーブルと初期化に使う大きな数
 INF = 1 << 30
-dp = [[INF] * 3 for _ in range(n + 1)]
-dp[0][0] = P[0]
-dp[0][1] = Q[0]
-dp[0][2] = R[0]
+dp = [[INF for _ in range(3)] for _ in range(N)]
 
-for i in range(n - 1):
-    dp[i + 1][0] = min(
-        dp[i][0] + P[i + 1] - a, dp[i][1] + Q[i + 1], dp[i][2] + R[i + 1]
-    )
-    dp[i + 1][1] = min(
-        dp[i][0] + P[i + 1], dp[i][1] + Q[i + 1] - a, dp[i][2] + R[i + 1]
-    )
-    dp[i + 1][2] = min(
-        dp[i][0] + P[i + 1], dp[i][1] + Q[i + 1], dp[i][2] + R[i + 1] - a
-    )
+# DP 初期条件
+for s in range(3):
+    dp[0][s] = S[s][0]
 
-print(min(dp[n - 1]))
+# DP テーブルの更新
+for i in range(1, N):
+    for s in range(3):
+        # dp[i][s] を求める
+        cost = INF
+        for ps in range(3):
+            # 前日に選んだ店 ps で場合分けをして、価格の総和の最小値を更新する
+            if ps == s:
+                cost = min(cost, dp[i - 1][ps] + S[s][i] - A)
+            else:
+                cost = min(cost, dp[i - 1][ps] + S[s][i])
+        dp[i][s] = cost
+
+# 答えを出力する
+ans = INF
+for s in range(3):
+    ans = min(ans, dp[N - 1][s])
+print(ans)
