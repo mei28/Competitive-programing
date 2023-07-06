a8d6ef5e24ad102ddd7c8c43f704c82daf0e3c02
mei <mei28aquarius@gmail.com>
2023-07-06 11:40:14 +0900

    a dd some

--- ABC-305/abc-305/src/bin/a.rs
+++ ABC-305/abc-305/src/bin/a.rs
@@ -6,9 +6,9 @@ fn main() {
     let a = n / 5;
     let b = (n + 5 - 1) / 5;
 
-    if n - a*5 >= b*5 - n {
-        println!("{}", b*5);
+    if n - a * 5 >= b * 5 - n {
+        println!("{}", b * 5);
     } else {
-        println!("{}", a*5);
+        println!("{}", a * 5);
     }
 }
--- ABC-305/abc-305/src/bin/c.rs
+++ ABC-305/abc-305/src/bin/c.rs
@@ -1,6 +1,29 @@
-use proconio::{fastout, input};
+use proconio::{fastout, input, marker::*};
+use std::cmp::{max, min};
 
 #[fastout]
 fn main() {
-    input! {}
+    input! {h:usize,w:usize,s: [Chars;h]}
+    let mut dr = (0, 0);
+    let mut ul = (h, w);
+
+    for i in 0..h {
+        for j in 0..w {
+            if s[i][j] == '#' {
+                dr.0 = std::cmp::max(dr.0, i);
+                dr.1 = std::cmp::max(dr.1, j);
+
+                ul.0 = std::cmp::min(ul.0, i);
+                ul.1 = std::cmp::min(ul.1, j);
+            }
+        }
+    }
+    for i in ul.0..=dr.0 {
+        for j in ul.1..=dr.1 {
+            if s[i][j] == '.' {
+                println!("{} {}", i + 1, j + 1);
+                return;
+            }
+        }
+    }
 }
