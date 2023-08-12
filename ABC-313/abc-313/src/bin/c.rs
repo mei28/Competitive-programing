ebb5f425622edb926da8a1019dd467a1009164ba
mei <mei28aquarius@gmail.com>
2023-08-12 20:55:31 +0900

    add

--- ABC-313/abc-313/src/bin/c.rs
+++ ABC-313/abc-313/src/bin/c.rs
@@ -2,5 +2,25 @@ use proconio::{fastout, input};
 
 #[fastout]
 fn main() {
-    input! {}
+    input! {n:usize,mut a:[usize;n]}
+
+    let mut sum = 0;
+    for i in 0..n {
+        sum += a[i];
+    }
+
+    a.sort();
+
+    let mut b = vec![sum / n; n];
+
+    for i in 0..(sum % n) {
+        b[n - 1 - i] += 1;
+    }
+
+    let mut ans = 0;
+
+    for i in 0..n {
+        ans += (a[i] as i64 - b[i] as i64).abs();
+    }
+    println!("{}", ans);
 }
