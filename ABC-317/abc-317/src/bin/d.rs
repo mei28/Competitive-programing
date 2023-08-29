7091edd63d16c9cb0eb918a74c89d3659ca6ba93
mei <mei28aquarius@gmail.com>
2023-08-29 18:09:52 +0900

    add some

--- ABC-317/abc-317/src/bin/d.rs
+++ ABC-317/abc-317/src/bin/d.rs
@@ -1,14 +1,33 @@
-use cmp::{max, min, Reverse};
-use proconio::{fastout, input, marker::*};
-use std::collections::*;
-use std::*;
+use proconio::{fastout, input};
+use std::cmp::{max, min};
 
-const INF: usize = 1000000;
+const INF: i64 = 1e18 as i64;
 
+#[fastout]
 fn main() {
     input! {
         n: usize,
+        xyz: [(i64, i64, i64); n]  // Using i64 here to match long long in C++
     }
 
-    println!("{}", n);
+    let z_sum = xyz.iter().map(|(_, _, z)| *z as usize).sum::<usize>();
+    let mut dp = vec![INF; z_sum + 1];
+
+    dp[0] = 0;
+
+    for &(x, y, z) in &xyz {
+        // Directly destructure tuple here
+        let w = max(0, (x + y) / 2 + 1 - x);
+
+        for j in (z as usize..=z_sum).rev() {
+            dp[j] = min(dp[j], dp[j - z as usize] + w);
+        }
+    }
+
+    let mut ans = INF;
+    for j in (z_sum / 2 + 1)..=z_sum {
+        ans = min(ans, dp[j]);
+    }
+    println!("{}", ans);
 }
+
