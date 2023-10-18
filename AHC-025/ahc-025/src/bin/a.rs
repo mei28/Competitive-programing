e64a5ccb4efb1ddf32bd30c7635887d1e9ba7076
mei <mei28aquarius@gmail.com>
2023-10-18 16:09:37 +0900

    add

--- AHC-025/ahc-025/src/bin/a.rs
+++ AHC-025/ahc-025/src/bin/a.rs
@@ -6,14 +6,13 @@ use std::io::{self, stdin, stdout, BufReader, Write};
 #[derive(Debug, Copy, Clone)]
 struct Item {
     weight: i32,
-    group: usize, // アイテムがどのグループに属しているか
+    group: usize,
 }
 
 fn query(
     left_group: usize,
     right_group: usize,
     items: &Vec<Item>,
-    flg: bool,
     query_count: &mut usize,
 ) -> (char, Vec<usize>, Vec<usize>) {
     let mut left: Vec<usize> = items
@@ -28,56 +27,36 @@ fn query(
         .filter(|&(_, item)| item.group == right_group)
         .map(|(index, _)| index)
         .collect();
-    if flg {
-        left = select_random_percentage(&left, 0.2);
-        right = select_random_percentage(&right, 0.2);
-    }
+    // 最軽と推定されるアイテムと最重と推定されるアイテムを選択
+    left.sort_by_key(|&index| items[index].weight);
+    right.sort_by_key(|&index| items[index].weight);
+    let left_samples = vec![left[0], left[left.len() - 1]];
+    let right_samples = vec![right[0], right[right.len() - 1]];
 
     // クエリを投げる
     println!(
         "{} {} {} {}",
-        left.len(),
-        right.len(),
-        left.iter()
+        left_samples.len(),
+        right_samples.len(),
+        left_samples
+            .iter()
             .map(|x| x.to_string())
             .collect::<Vec<String>>()
             .join(" "),
-        right
+        right_samples
             .iter()
             .map(|x| x.to_string())
             .collect::<Vec<String>>()
             .join(" ")
     );
+
     *query_count += 1;
-    // 出力をフラッシュする
     io::stdout().flush().unwrap();
 
     let mut stdin = LineSource::new(BufReader::new(io::stdin()));
     macro_rules! input(($($tt:tt)*) => (proconio::input!(from &mut stdin, $($tt)*)));
-    // 結果を受け取る
     input! {c:char}
-    (c, left, right) // 選択された左側と右側のアイテムのインデックスも返します
-}
-
-fn swap_specific_items(
-    left_indices: &Vec<usize>,
-    right_indices: &Vec<usize>,
-    items: &mut Vec<Item>,
-) {
-    for &index in left_indices.iter() {
-        items[index].group = items[right_indices[0]].group;
-    }
-    for &index in right_indices.iter() {
-        items[index].group = items[left_indices[0]].group;
-    }
-}
-
-fn select_random_percentage<T: Clone>(items: &Vec<T>, percentage: f32) -> Vec<T> {
-    let count = (items.len() as f32 * percentage).ceil() as usize;
-    items
-        .choose_multiple(&mut rand::thread_rng(), count)
-        .cloned()
-        .collect()
+    (c, left_samples, right_samples)
 }
 
 fn show_ans(items: &Vec<Item>, debug: bool) {
@@ -86,10 +65,52 @@ fn show_ans(items: &Vec<Item>, debug: bool) {
         print!("#c ");
     }
     println!("{}", answer.join(" "));
-    // 出力をフラッシュする
     io::stdout().flush().unwrap();
 }
 
+fn swap_specific_items(
+    left_indices: &Vec<usize>,
+    right_indices: &Vec<usize>,
+    items: &mut Vec<Item>,
+) {
+    let left_group = items[left_indices[0]].group; // 左側のアイテムのグループを取得
+    let right_group = items[right_indices[0]].group; // 右側のアイテムのグループを取得
+
+    // 左側のアイテムのグループを右側のアイテムのグループに更新
+    for &index in left_indices.iter() {
+        items[index].group = right_group;
+    }
+
+    // 右側のアイテムのグループを左側のアイテムのグループに更新
+    for &index in right_indices.iter() {
+        items[index].group = left_group;
+    }
+}
+
+fn reassign_groups_based_on_weight(items: &mut Vec<Item>, d: usize) {
+    let mut indexed_weights: Vec<(i32, usize)> = items
+        .iter()
+        .enumerate()
+        .map(|(idx, item)| (item.weight, idx))
+        .collect();
+    indexed_weights.sort_by(|a, b| a.0.cmp(&b.0));
+
+    // 各グループの合計重さを格納
+    let mut group_sums: Vec<i32> = vec![0; d];
+
+    // ソートされたアイテムのリストを逆順にイテレート
+    for (_, original_idx) in indexed_weights.iter().rev() {
+        // 現在のアイテムを最も軽いグループに割り当て
+        let min_group_idx = group_sums
+            .iter()
+            .enumerate()
+            .min_by_key(|&(_, sum)| sum)
+            .unwrap()
+            .0;
+        items[*original_idx].group = min_group_idx;
+        group_sums[min_group_idx] += items[*original_idx].weight;
+    }
+}
 fn main() {
     let mut stdin = LineSource::new(BufReader::new(io::stdin()));
     macro_rules! input(($($tt:tt)*) => (proconio::input!(from &mut stdin, $($tt)*)));
@@ -98,24 +119,37 @@ fn main() {
     let mut items = Vec::with_capacity(n);
     for i in 0..n {
         items.push(Item {
-            weight: -1,
-            group: i % d, // これにより、各アイテムは 0 から d-1 までの異なるグループIDを持ちます
+            weight: 0,
+            group: i % d,
         });
     }
     let mut query_count = 0;
-    let mut ans1 = '='; // 初期値を設定
-    let mut selected_left = Vec::new();
-    let mut selected_right = Vec::new();
     while query_count < q {
-        show_ans(&items, true);
         let group_a = query_count % d;
         let group_b = (query_count + 1) % d;
 
-        (ans1, selected_left, selected_right) =
-            query(group_a, group_b, &items, true, &mut query_count);
-        swap_specific_items(&selected_left, &selected_right, &mut items);
+        let (ans1, selected_left_1, selected_right_1) =
+            query(group_a, group_b, &items, &mut query_count);
+
+        if ans1 == '<' {
+            items[selected_left_1[0]].weight -= 1;
+            items[selected_left_1[1]].weight += 1;
+            items[selected_right_1[0]].weight += 1;
+            items[selected_right_1[1]].weight -= 1;
+
+            swap_specific_items(&selected_left_1, &selected_right_1, &mut items);
+        } else {
+            items[selected_left_1[0]].weight += 1;
+            items[selected_left_1[1]].weight -= 1;
+            items[selected_right_1[0]].weight -= 1;
+            items[selected_right_1[1]].weight += 1;
+
+            swap_specific_items(&selected_right_1, &selected_left_1, &mut items);
+        }
+
+        reassign_groups_based_on_weight(&mut items, d);
     }
 
-    // TODO: 分割した答えを投げる
+    reassign_groups_based_on_weight(&mut items, d);
     show_ans(&items, false);
 }
