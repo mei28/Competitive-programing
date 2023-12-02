07a9540c947ca357af6621b9b2a9be20c84e73f1
mei <mei28aquarius@gmail.com>
2023-12-02 15:04:48 +0900

    add some'

--- orediff-006/src/bin/b.rs
+++ orediff-006/src/bin/b.rs
@@ -2,5 +2,11 @@ use proconio::{fastout, input};
 
 #[fastout]
 fn main() {
-    input! {}
+    input! {n:f64}
+    let mut ans: f64 = 0.0;
+
+    for i in 1..n as usize {
+        ans += (n / i as f64);
+    }
+    println!("{}", ans);
 }
