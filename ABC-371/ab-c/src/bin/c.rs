cb8a9ae8af3b9bd8698681429ef7d95b4a69cc23
mei <mei28aquarius@gmail.com>
2024-09-19 17:24:46 +0900

    add

--- ABC-371/ab-c/src/bin/c.rs
+++ ABC-371/ab-c/src/bin/c.rs
@@ -1,6 +1,54 @@
+use permutohedron::LexicalPermutation;
 use proconio::input;
+use std::collections::HashSet;
 
 fn main() {
-    input! {}
+    input! {n:usize, m_g: usize}
+    let mut edges_g: HashSet<(usize, usize)> = HashSet::new();
+    let mut edges_h: HashSet<(usize, usize)> = HashSet::new();
+
+    for _ in 0..m_g {
+        input! {u:usize, v:usize}
+        edges_g.insert((u - 1, v - 1));
+        edges_g.insert((v - 1, u - 1));
+    }
+
+    input! {m_h: usize}
+    for _ in 0..m_h {
+        input! {u:usize, v:usize}
+        edges_h.insert((u - 1, v - 1));
+        edges_h.insert((v - 1, u - 1));
+    }
+
+    let mut a = vec![vec![0; n]; n];
+    for i in 0..n {
+        for j in i + 1..n {
+            input! {b:usize}
+            a[i][j] = b;
+            a[j][i] = b;
+        }
+    }
+
+    let mut p = (0..n).collect::<Vec<usize>>();
+    let mut ans = 28000000; // 初期値は十分大きい値に設定
+
+    loop {
+        let mut sum = 0;
+        for i in 0..n {
+            for j in 0..i {
+                let h_contains = edges_h.contains(&(i, j));
+                let g_contains = edges_g.contains(&(p[i], p[j]));
+                if h_contains != g_contains {
+                    sum += a[i][j];
+                }
+            }
+        }
+        ans = ans.min(sum);
+
+        if !p.next_permutation() {
+            break;
+        }
+    }
+    println!("{}", ans);
 }
 
--- ABC-371/ab-c/src/bin/d.rs
+++ ABC-371/ab-c/src/bin/d.rs
@@ -1,6 +1,34 @@
 use proconio::input;
+use superslice::Ext;
 
 fn main() {
-    input! {}
+    input! {
+        n: usize,
+        mut xs: [i32; n],
+        ps: [i32; n],
+        q: usize,
+        lr: [(i32, i32); q],
+    }
+
+    // xsに対応したpsの累積和を計算する
+    let mut acc = vec![0; n + 1];
+    for i in 0..n {
+        acc[i + 1] = acc[i] + ps[i]; // 累積和
+    }
+
+    // xsをソートする
+    let mut sorted_xs = xs.clone();
+    sorted_xs.sort();
+
+    // 各範囲について累積和の計算
+    for &(l, r) in &lr {
+        // l以上の最初のインデックスと、r以下の最後のインデックスを二分探索で見つける
+        let left = sorted_xs.lower_bound(&l);
+        let right = sorted_xs.upper_bound(&r);
+
+        // 累積和の差分で区間の合計を計算
+        let sum = acc[right] - acc[left];
+        println!("{}", sum);
+    }
 }
 
